a04b3277c16ffbda8817b3ebb17f2d567a9fd474
Mostafa Habib <momosasahabib9@gmail.com>
2024-12-26 01:31:34 +0200

    Video Part 02 Interface - Example 01

---
+++ Demo/Interface Example 01/IMyType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interface_Example_01
+{
+    internal interface IMyType
+    {
+        public int MyProperty {  get; set; }
+
+        public void MyFunction();
+
+        public void Print()
+        {
+            Console.WriteLine("Hello Default Implemented method");
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Demo
+﻿using Demo.Interface_Example_01;
+
+namespace Demo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            IMyType myType;
+
+            MyType myType1 = new MyType();
+            myType1.MyProperty = 10;
+            myType1.MyFunction();
+
+            IMyType refFromInterface = new MyType();
+            refFromInterface.MyProperty = 20;
+            refFromInterface.MyFunction();
+            refFromInterface.Print();
         }
     }
 }
