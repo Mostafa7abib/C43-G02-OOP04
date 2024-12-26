ea75382b7b468dc4b2f3c007335be72568a0f17d
Mostafa Habib <momosasahabib9@gmail.com>
2024-12-26 18:38:22 +0200

    Video Part 10 Operators Overloading - Binary Operators

---
+++ Demo/Operators Overloading/Complex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Operators_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+
+        #region Opeartor Overloading
+        public static Complex operator + (Complex Left, Complex Right)
+        {
+            return new Complex()
+            {
+                Real = Left.Real + Right.Real,
+                Imag = Left.Imag + Right.Imag
+            };
+        }
+        #endregion
+        public override string ToString()
+        {
+            return  $"{Real} + {Imag}i"; 
+        }
+    }
+}
