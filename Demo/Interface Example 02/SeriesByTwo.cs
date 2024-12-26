84063f554211c591f38a25e200f2309ed2137e27
Mostafa Habib <momosasahabib9@gmail.com>
2024-12-26 13:12:24 +0200

    Video Part 03 Interface - Example 02

---
+++ Demo/Interface Example 02/SeriesByTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interface_Example_02
+{
+    internal class SeriesByTwo : ISeries
+    {
+        public int Current { get; set; }
+
+        public void GetNext()
+        {
+            Current += 2;
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,21 +1,43 @@
 ﻿using Demo.Interface_Example_01;
+using Demo.Interface_Example_02;
 
 namespace Demo
 {
     internal class Program
     {
+        static void PrintTenNumbersFromSeries(ISeries series)
+        {
+            if (series is null)
+                return;
+            for (int i = 0; i<= 10; i++)
+            {
+                Console.Write($"{series.Current} \t");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
         static void Main()
         {
-            IMyType myType;
+            #region Video 2
+            //IMyType myType;
 
-            MyType myType1 = new MyType();
-            myType1.MyProperty = 10;
-            myType1.MyFunction();
+            //MyType myType1 = new MyType();
+            //myType1.MyProperty = 10;
+            //myType1.MyFunction();
 
-            IMyType refFromInterface = new MyType();
-            refFromInterface.MyProperty = 20;
-            refFromInterface.MyFunction();
-            refFromInterface.Print();
+            //IMyType refFromInterface = new MyType();
+            //refFromInterface.MyProperty = 20;
+            //refFromInterface.MyFunction();
+            //refFromInterface.Print();
+            #endregion
+
+            SeriesByTwo seriesByTwo = new SeriesByTwo();
+            PrintTenNumbersFromSeries(seriesByTwo);
+            SeriesByThree seriesByThree = new SeriesByThree();  
+            PrintTenNumbersFromSeries(seriesByThree);
+            SeriesByFour seriesByFour = new SeriesByFour();
+            PrintTenNumbersFromSeries(seriesByFour);
         }
-    }
+    } 
 }
