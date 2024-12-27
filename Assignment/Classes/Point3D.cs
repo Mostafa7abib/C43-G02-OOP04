e0148ed7d6aeb479cfd9f3313f6aa2323b4ed32c
Mostafa Habib <momosasahabib9@gmail.com>
2024-12-27 03:27:17 +0200

    Part 2

---
+++ Assignment/Classes/Point3D.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Classes
+{
+    internal class Point3D : IComparable, ICloneable
+
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+        public Point3D() : this(0, 0, 0) { }
+        public Point3D(int x, int y) : this(x, y, 0) { }
+        public Point3D(int x, int y, int z)
+        {
+            Num1 = x;
+            Num2 = y;
+            Num3 = z;
+        }
+        public override string ToString()
+        {
+            return $"Point Coordinates: {Num1} , {Num2} , {Num3}";
+        }
+
+        public object Clone()
+        {
+            return new Point3D(Num1, Num2, Num3);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point3D other)
+                return Num1 == other.Num1 && Num2 == other.Num2 && Num3 == other.Num3;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return (Num1, Num2, Num3).GetHashCode();
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Point3D other)
+            {
+                if (Num1 != other.Num1)
+                    return Num1.CompareTo(other.Num1);
+                if (Num2 != other.Num2)
+                    return Num2.CompareTo(other.Num2);
+                else return Num3.CompareTo(other.Num3);
+            }
+            throw new ArgumentException("Invalid!");
+        }
+
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            return left?.Equals(right) ?? right is null;
+        }
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            return !(left == right);
+        }
+
+
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -22,13 +22,63 @@ namespace Assignment
             #endregion
 
             #region Part1 Q2
-            INotificationService Email_notification = new EmailNotificationService();
-            INotificationService Sms_notification = new SmsNotificationService();
-            INotificationService Push_notification = new PushNotificationService();
-            Email_notification.SendNotification("mostafamohabib1234@gmail.com", "This Message Via An Email!");
-            Sms_notification.SendNotification("Et Cash", "This Message Via An Sms!");
-            Push_notification.SendNotification("sasa", "This Message Via An Push Noification!");
+            //INotificationService Email_notification = new EmailNotificationService();
+            //INotificationService Sms_notification = new SmsNotificationService();
+            //INotificationService Push_notification = new PushNotificationService();
+            //Email_notification.SendNotification("mostafamohabib1234@gmail.com", "This Message Via An Email!");
+            //Sms_notification.SendNotification("Et Cash", "This Message Via An Sms!");
+            //Push_notification.SendNotification("sasa", "This Message Via An Push Noification!");
             #endregion
+
+            try
+            {
+                Console.WriteLine("Enter Cordinate for P1:");
+                Point3D p1 = ReadPoint();
+                Console.WriteLine("Enter Cordinate for P2:");
+                Point3D p2 = ReadPoint();
+                Console.WriteLine($"P1: {p1}");
+                Console.WriteLine($"P2: {p2}");
+                Console.WriteLine($"P1 == P2 ? {p1 == p2}!");
+                Point3D[] points = new Point3D[]
+                {
+                    new Point3D(1,2,3),
+                    new Point3D(4,5,6),
+                    new Point3D(10,3,4),
+                    new Point3D(7,8,9)
+                };
+                Array.Sort(points);
+                foreach (var point in points)
+                {
+                    Console.WriteLine(point);
+                }
+                Point3D p3 = (Point3D)p1.Clone();
+                Console.WriteLine($"P3: {p3}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Erorr! Try Again Later");
+            }
         }
+        private static Point3D ReadPoint()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                if (parts.Length == 3 &&
+                    int.TryParse(parts[0], out int Num1) &&
+                    int.TryParse(parts[1], out int Num2) &&
+                    int.TryParse(parts[2], out int Num3))
+                {
+                    return new Point3D(Num1, Num2, Num3);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+
     }
 }
+
