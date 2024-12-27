9320a46d1c5c0694f149c68594188b1a0b7ced7c
Mostafa Habib <momosasahabib9@gmail.com>
2024-12-27 03:01:14 +0200

    Part 1 Q1

---
+++ Assignment/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Interfaces
+{
+    internal interface IAuthenticationService
+    {
+        public bool AuthenticateUser(string UserName, string Password);
+        public bool AuthorizeUser(string UserName, string Role);
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using Assignment.Classes;
+using Assignment.Interfaces;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 1 Q1 
+            IAuthenticationService service = new AuthenticationService();
+            IAuthenticationService Authorize = new AuthenticationService();
+            bool Authenticated = service.AuthenticateUser("user", "passowrd");
+            if (Authenticated)
+                Console.WriteLine($"Authenticated: {Authenticated}");
+            else Console.WriteLine("UnAthenticated!");
+            bool Authorized = Authorize.AuthorizeUser("user2", "admin");
+            if (Authorized)
+                Console.WriteLine($"Authorized: {Authorized}");
+            else Console.WriteLine("UnAuthorized!");
+
+            #endregion
+        }
+    }
+}
